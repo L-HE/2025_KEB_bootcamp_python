58d46a585a3b16f60ec8cf8c9565991889c97772
lhe <gsurplus@inha.edu>
2025-02-11 13:31:36 +0900

    v0.8 fucntion & help

--- day02.py
+++ day02.py
@@ -1,17 +1,24 @@
-import math
+def is_prime(number) -> bool:
+    """
+    A function that determines whether a prime number is present
+    and returns True if it is a prime number and False if it is not a prime number
+    :param num: integer number
+    :return: boolean type
+    """
+    if number >= 2:
+        for i in range(2, int(number**0.5) + 1):          #n**0.5 => n의 0.5제곱
+            if number % i == 0:
+                return False
+    else:
+        return False
+    return True
+
+# main
+help(is_prime)
 
 number = int(input ("Input number: "))
-is_prime = True
-if number >= 2:
-    for i in range(2, int(math.sqrt(number)) + 1):          #n**0.5 => n의 0.5제곱
-        if number % i == 0:
-            is_prime = False            #count = count + 1
-            break                       #숫자가 커질수록 break의 의미가 커진다
-        print(i, end=' ')
-else:
-    is_prime = False
 
-if is_prime:                            #if count == 0:
+if is_prime(number):
     print(f"{number} is prime number")
 else:
     print(f"{number} is NOT prime number")
