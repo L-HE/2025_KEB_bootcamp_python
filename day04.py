0768cc6ed0b11131ee241c2d4c3d63fababa8fcf
lhe <gsurplus@inha.edu>
2025-02-13 14:17:30 +0900

    v2.4 time.time()

--- day04.py
+++ day04.py
@@ -2,12 +2,18 @@
 # S : 단일 책임의 원칙
 # Open Closed Principle : 개방 폐쇄 원칙 (확장에는 열려있고 수정에는 닫혀있는 원칙)
 
+#시간을 기록할 수 있는 모듈
+import  time
+
 
 def factorial_repetition(num) -> int :
     result = 1
-    for i in range(2, n + 1):
+    for i in range(2, num + 1):
         result = result * i
     return result
 
 number = int(input())
+s = time.time()
 print(f"{number}! = {factorial_repetition(number)}")
+e = time.time()
+print(e-s)
