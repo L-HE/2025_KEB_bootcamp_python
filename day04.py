4629793bb0b15458130d55dddc2b81607a5d7ecf
lhe <gsurplus@inha.edu>
2025-02-13 16:25:50 +0900

    v2.7 double decorator

--- day04.py
+++ day04.py
@@ -1,13 +1,33 @@
-# SOLID
-# S : 단일 책임의 원칙
-# Open Closed Principle : 개방 폐쇄 원칙 (확장에는 열려있고 수정에는 닫혀있는 원칙)
+# Assignment
+# v2.7) 2중 데코레이터 적용. time_decorator + description_decorator -> factorial 함수에 적용.
 
 #시간을 기록할 수 있는 모듈
 import  time
 
 
+def description_decorator(func):
+    """
+    dd
+    """
+    def inner(*arg):
+        """
+        inner
+        """
+        print(func.__name__)
+        print(func.__doc__)
+        r = func(*arg)
+        return r
+    return inner
+
+
 def time_decorator(func):
+    """
+    td
+    """
     def wrapper(*arg):
+        """
+        wrapper
+        """
         s = time.time()
         r = func(*arg)
         e = time.time()
@@ -16,15 +36,19 @@ def time_decorator(func):
     return  wrapper
 
 
-#@time_decorator
+@time_decorator
+@description_decorator
 def factorial_repetition(num) -> int :
+    """
+    f_r
+    """
     result = 1
     for i in range(2, num + 1):
         result = result * i
     return result
 
 number = int(input())
-ft = time_decorator(factorial_repetition)
-print(f"{number}! = {ft(number)}")
-number = int(input())
+# dd = description_decorator(time_decorator(factorial_repetition))
+# print(f"{number}! = {dd(number)}")
 print(f"{number}! = {factorial_repetition(number)}")
+
