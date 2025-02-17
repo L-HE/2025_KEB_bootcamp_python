d7a834fd0d7585737380e781d1f4a6cb6bfd54fa
lhe <gsurplus@inha.edu>
2025-02-17 19:47:48 +0900

    v3.2 Assignment3 Fibonacci

--- day05.py
+++ day05.py
@@ -1,21 +1,39 @@
-# Assignment02
-# v3.1) Factorial
+# Assignment3
+# v3.2) Fibonacci Number with memoization
 
 
-def factorial_loop(num) -> int:
-    answer = 1
-    for i in range(2, num + 1):
-        answer = answer * i
-    return answer
-
-
-def factorial_recursion(num) -> int:
-    if num == 0:
-        return 1
+def fibonacci_loop(num) -> int:
+    """
+    반복문을 통해, 입력받은 수를 피보나치 수로 변환해주는 함수
+    :param num: 입력받은 수
+    :return: 입력받은 수에 해당하는 피보나치 수
+    """
+    flist = [0, 1]
+    if num <= 1:
+        return flist[num]
     else:
-        return num * factorial_recursion(num - 1)
+        for i in range(2, num + 1):
+            flist.append(flist[i - 2] + flist[i - 1])
+        return flist[num]
 
 
-num = int(input('Insert a number: '))
-print(f'{num}! = {factorial_loop(num)}')
-print(f'{num}! = {factorial_recursion(num)}')
+def fibonacci_recursion(num) -> int:
+    """
+    재귀함수와 memoization을 이용해, 입력받은 수를 피보나치 수로 변환하는 함수
+    :param num: 입력받은 수
+    :return: 입력받은 수에 해당하는 피보나치 수
+    """
+    memo = {}
+
+    if num in memo:
+        return memo[num]
+    elif num <= 1:
+        return num
+    else:
+        memo = {num : fibonacci_recursion(num - 2) + fibonacci_recursion(num - 1)}
+        return memo[num]
+
+
+num = int(input("Insert a number: "))
+print(fibonacci_loop(num))
+print(fibonacci_recursion(num))
